41f7b3df7eab3c7fca681b8830e314e0571b268f
MyleneVinci <mylene.vinci@hotmail.com>
2022-10-18 11:04:27 +0200

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,8 +5,7 @@ import logo from "../../assets/logo.png"
 import './navbar.css';
 
 const Navbar = () => {
-    let iconStyle = { fontSize: "28px", color: '#F7F2D4' };
-
+  let iconStyle = { fontSize: "28px", color: '#F7F2D4' };
 
   return (
     <div className='navbar'>
@@ -16,22 +15,19 @@ const Navbar = () => {
                     <img classname='logo' src={logo} alt="logo" />
                 </div>
                 <div className='navbar-menu'>
-                        <FaRegEnvelope style={iconStyle} />
-                        <FaRegUser style={iconStyle} />
+                    <FaRegEnvelope style={iconStyle} />
+                    <FaRegUser style={iconStyle} />
                 </div>
             </div>
             <div className='header'></div>
-
             <div className='bottom-navbar'>
                 <FaHome style={iconStyle} />
                 <FaRegEdit style={iconStyle} />
                 <FaRegMap style={iconStyle} />
                 <FaInfoCircle style={iconStyle} />
             </div>
-
         </div>
         <div className='desktop-navbar'>
-
         </div>
     </div>
   )
