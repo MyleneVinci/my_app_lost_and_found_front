86471808c6459a57f0c868121bc187495ab30224
Mylene Vinci <mylene.vinci@hotmail.com>
2022-12-22 11:37:44 +0100

    display ad for lost and found items on profile

--- src/components/profileComponents/LostItems.jsx
+++ src/components/profileComponents/LostItems.jsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect }  from 'react';
+import { useParams } from 'react-router-dom';
+
+import axios from 'axios';
+
 
 import ProfileCard from './profileCard/ProfileCard';
 
 const LostItems = () => {
+
+  //gestion des appels a l'api
+  const url = `${process.env.REACT_APP_API_URL}`
+
+
+  //affichage des données de la base de données
+  const { id }= useParams();
+  const [getData, setGetData] = useState([]);
+
+  useEffect(() => {
+      axios
+          .get(`${process.env.REACT_APP_API_URL}/user/ad/${id}`)
+          .then((res) => res.data)
+          .then((data) => setGetData(data))
+  }, [id])
+  
   return (
     <div>
         <ProfileCard 
           title='Mes objets perdus'
+          getDataAd= {getData}
         />
     </div>
   )
--- src/components/profileComponents/profileCard/ProfileCard.jsx
+++ src/components/profileComponents/profileCard/ProfileCard.jsx
@@ -4,13 +4,20 @@ import { FaChevronDown, FaChevronUp  } from "react-icons/fa";
 import './profileCard.css';
 
 
-const ProfileCard = ({title}) => {
+const ProfileCard = ({title, getDataAd}) => {
 
+    //gestion menu déroulant
     const [isTitleReduce, setIsTitleReduce] = useState(false);
 
     const handleTitleReduce = () => {
         setIsTitleReduce(!isTitleReduce);
-        }
+    }
+
+    //format de la date
+    const newFormatDate = (date) => {
+        return date.substring(0, 10).split("-").reverse().join("-");  
+    }
+  
     
     return (
         <div className='profile-block'>
@@ -18,8 +25,22 @@ const ProfileCard = ({title}) => {
                 <h2>{title}</h2>
                 <span onClick={handleTitleReduce}>{isTitleReduce ? <FaChevronUp/> : <FaChevronDown/>}</span>
             </div>
-            { isTitleReduce ? <p>pas d'informations pour le moment</p> : null }       
-        </div>
+            { isTitleReduce ? 
+                <div className="data-card-content-list">
+                {
+                    getDataAd &&
+                    getDataAd.map((ad, index) => (
+                        <div className='list-ad' key={index}>
+                            <ul>
+                                <li>Numéro de l'annonce{index+1}</li>
+                                <li>Date de publication : {newFormatDate(ad.date)}</li>
+                                <li>Type d'objet : {ad.type}</li>
+                            </ul>
+                        </div>
+                    ))
+                }
+            </div> : null }
+    </div>
     )
 }
 
