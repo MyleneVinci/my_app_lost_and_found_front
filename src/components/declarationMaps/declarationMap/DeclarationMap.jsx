7d0f0c65ab255f1b1dcc64ac296b325e81cda2fc
Mylene Vinci <mylene.vinci@hotmail.com>
2022-12-22 11:48:02 +0100

    change classname

--- src/components/declarationMaps/declarationMap/DeclarationMap.jsx
+++ src/components/declarationMaps/declarationMap/DeclarationMap.jsx
@@ -91,7 +91,7 @@ const MyLocation = () => {
     }
 
     return (
-        <div className="mapContainer">
+        <div className="declarationMapContainer">
             <MapContainer center={[43.296482, 5.36978]} zoom={13} scrollWheelZoom={false}
             > 
                 <TileLayer
